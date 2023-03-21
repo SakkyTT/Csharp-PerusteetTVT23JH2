f21522a4080688ebd6cc19c55d72009fb0ce4d7a
SakkyTT <teemu.turunen@sakky.fi>
2023-03-21 13:59:59 +0200

    first commit

---
+++ Opetus21.3.Datarakenteet/Opetus27.3/Opetus27.3/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Opetus27._3_
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
