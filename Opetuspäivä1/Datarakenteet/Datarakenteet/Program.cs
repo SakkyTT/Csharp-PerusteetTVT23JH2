f0c2eaf2ee7df1eea6e95e75682ed6a50bb74888
SakkyTT <teemu.turunen@sakky.fi>
2023-03-21 14:04:14 +0200

    renamed files

---
+++ Opetuspäivä1/Datarakenteet/Datarakenteet/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Datarakenteet
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
