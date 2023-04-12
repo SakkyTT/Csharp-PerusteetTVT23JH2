6f1bd61543e67ebfe6632fe9843125b00fa0e51e
TeemuPTurunen <teemu.turunen@sakky.fi>
2023-04-12 19:43:42 +0300

    // 34D C# String: Etsi kirjaimia merkkijonosta - valmis

--- Opetuspäivä7Harjoitukset/Opetuspäivä7Harjoitukset/Program.cs
+++ Opetuspäivä7Harjoitukset/Opetuspäivä7Harjoitukset/Program.cs
@@ -48,43 +48,91 @@ namespace Opetuspäivä7Harjoitukset
             // Lista versio
             // Tässä versiossa käyttäjä voi tallentaa niin monta lukua kuin haluaa.
 
-            Console.WriteLine("Syötä lukuja (e to exit): ");
-            List<int> values = new List<int>();
-            bool userIsDone = false;
+            //Console.WriteLine("Syötä lukuja (e to exit): ");
+            //List<int> values = new List<int>();
+            //bool userIsDone = false;
 
-            // Silmukka, jossa käyttäjä voi syöttää haluamansa verran lukuja.
-            while (userIsDone == false) // tai do while
-            {
-                string userInput = Console.ReadLine();
+            //// Silmukka, jossa käyttäjä voi syöttää haluamansa verran lukuja.
+            //while (userIsDone == false) // tai do while
+            //{
+            //    string userInput = Console.ReadLine();
 
-                if (userInput == "e") // Tarkistetaan, jos käyttäjä lopettaa lukujen syötön
-                {
-                    userIsDone = true;
-                }
-                else // Muuten lisätään luku listaan.
-                {
-                    values.Add(int.Parse(userInput));
-                }
-            }
+            //    if (userInput == "e") // Tarkistetaan, jos käyttäjä lopettaa lukujen syötön
+            //    {
+            //        userIsDone = true;
+            //    }
+            //    else // Muuten lisätään luku listaan.
+            //    {
+            //        values.Add(int.Parse(userInput));
+            //    }
+            //}
 
-            // Listan summaus
-            Console.WriteLine($"Listan lukujen summa on: {values.Sum()}");
+            //// Listan summaus
+            //Console.WriteLine($"Listan lukujen summa on: {values.Sum()}");
 
-            int sumList = 0;
+            //int sumList = 0;
 
-            foreach (int number in values)
-            {
-                sumList += number;
-            }
+            //foreach (int number in values)
+            //{
+            //    sumList += number;
+            //}
 
-            Console.WriteLine($"Listan lukujen summa on: {sumList}");
+            //Console.WriteLine($"Listan lukujen summa on: {sumList}");
 
             #endregion
 
 
 
+
+            // 34D C# String: Etsi kirjaimia merkkijonosta (Yhdessä päivä 7)
+            // https://moodle.sakky.fi/mod/assign/view.php?id=1159657
             #region
 
+            Console.Write("Syötä sana 1: ");
+            string word1 = Console.ReadLine(); // "kuppi"
+
+            Console.Write("Syötä sana 2: ");
+            string word2 = Console.ReadLine(); // "kauppa"
+
+            // Taulukon käytön ongelmat:
+            // 1. Ei voi tietää kuinka pitkä sana on
+            // 2. Ei voi tietää montako merkkiä ovat samoja
+
+            // TODO: Luo taulukkon pituus tarkalleen oikein.
+
+            //charsInCommon sisältää merkit, jotka ilmenevät molemmissa sanoissa ja montako kertaa ne ilmenee
+            // Tässä datatyyppi on "Tuple" johon voi tallentaa kaksi eri datatyyppiä yhdessä
+            (char charValue, int intValue)[] charsInCommon = new (char, int)[word1.Length];
+
+            string charsTested = "";
+
+            // Luodaan silmukka, joka käy läpi kaikki word1 merkit ja tarkistetaan ilmeneekö se word2-muuttujassa
+            for (int i = 0; i < word1.Length; i++) // word1 indeksi == i
+            {
+                int numberOftimesFound = 0;
+
+                for (int j = 0; j < word2.Length; j++) // word2 indeksi == j
+                {
+                    // Onko sanan 1 indeksissä i sama kirjain kuin sanan 2 indeksissä j
+                    // Ja ei ole vielä tallennettu kirjainta taulukkoon
+                    if (word1[i] == word2[j] && charsTested.Contains(word1[i]) == false) 
+                    {
+                        // Estetään saman kirjaimen tallennus uudestaan
+
+                        numberOftimesFound++;
+                        charsInCommon[i] = (word1[i], numberOftimesFound);
+                    }
+                }
+                // Lisätään löydetty merkki, vasta sisemmän silmukan jälkeen.
+
+                charsTested += word1[i];
+            }
+
+            // Silmukka, jossa käydään läpi taulukko löydetyistä kirjaimista
+            foreach( (char, int) pair in charsInCommon)
+            {
+                Console.WriteLine($"Merkki {pair.Item1} löytyi {pair.Item2} kerran/kertaa");
+            }
 
             #endregion
 
