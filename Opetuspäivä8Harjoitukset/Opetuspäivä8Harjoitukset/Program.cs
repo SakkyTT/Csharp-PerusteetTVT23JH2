f037448516c61c1817fdd91f9244ce66b28b9019
SakkyTT <teemu.turunen@sakky.fi>
2023-04-17 19:33:07 +0300

    37 added

--- Opetuspäivä8Harjoitukset/Opetuspäivä8Harjoitukset/Program.cs
+++ Opetuspäivä8Harjoitukset/Opetuspäivä8Harjoitukset/Program.cs
@@ -29,11 +29,11 @@ namespace Opetuspäivä8Harjoitukset
 
             // List<datatyyppi> nimi = new List<datatyyppi>();
             List<char> vocalsUsingList = new List<char>(); // Lista
-            // Lista on oletuksena tyhjä ja sen pituus/Count on 0
-            // vocalsUsingList[0] = 'n'; // Kaatuu, ArgumentOutOfRangeException, koska lista on tyhjä
-            // Sen elementtejä ei voida vielä käyttää
+                                                           // Lista on oletuksena tyhjä ja sen pituus/Count on 0
+                                                           // vocalsUsingList[0] = 'n'; // Kaatuu, ArgumentOutOfRangeException, koska lista on tyhjä
+                                                           // Sen elementtejä ei voida vielä käyttää
 
-            
+
 
             // 3.Käy sanan merkit läpi silmukassa
 
@@ -84,6 +84,83 @@ namespace Opetuspäivä8Harjoitukset
             {
                 Console.WriteLine("Lista: " + vocalsUsingList[i]);
             }
+            #endregion
+
+
+            // 37 C# String: Lämpötilan seuranta
+            // https://moodle.sakky.fi/mod/assign/view.php?id=1159662
+            #region
+
+            // 1.Luo taulukko ja lista arvoille.
+
+            string[] measurementTimes = { "6:00", "12:00", "18:00", "24:00" };            
+
+            double[] temperaturesArray = new double[measurementTimes.Length]; // Taulukko
+            List<double> temperaturesList = new List<double>(); // Lista
+
+            // 2.Lue silmukka, jossa käyttäjän arvot luetaan ja tallennetaan taulukkoon
+            // ja listaan. (Arvo luetaan vain kerran per kierros ja se
+            // tallennetaan sekä taulukkoon ja listaan)
+            
+            for (int i = 0; i < measurementTimes.Length; i++ )
+            {
+                Console.Write($"Syötä kello {measurementTimes[i]} lämpötila: ");
+                double input = double.Parse(Console.ReadLine());
+                temperaturesArray[i] = input; // Taulukko
+                temperaturesList.Add(input); // Lista
+            }
+
+            // 3a.Ilmoita suurin, pienin ja keskilämpötila käyttämällä valmiita metodeja.
+            // 4.Tulosta suurin, pienin ja keskilämpötila käyttäjälle.
+            Console.WriteLine($"Taulukon suurin lämpötila on: {temperaturesArray.Max()}");
+            Console.WriteLine($"Taulukon pienin lämpötila on: {temperaturesArray.Min()}");
+            Console.WriteLine($"Taulukon keskilämpötila on: {temperaturesArray.Average()}");
+
+            Console.WriteLine($"Listan suurin lämpötila on: {temperaturesList.Max()}");
+            Console.WriteLine($"Listan pienin lämpötila on: {temperaturesList.Min()}");
+            Console.WriteLine($"Listan keskilämpötila on: {temperaturesList.Average()}");
+
+            // 3b.Suorita arvojen etsiminen omalla koodilla. (Kysy esim ChatGPT:
+            // How to find the smallest value in an array using for loop)
+
+
+            // Algoritmi, termi koodille, joka suorittaa jonkin asian
+            // Tässä algoritmi, joka laskee keskiarvon.
+            int count = 0;
+            double sum = 0;
+
+            foreach (double temp in temperaturesArray)
+            {
+                sum += temp;
+                count++;
+            }
+
+            Console.WriteLine($"Taulukon keskiarvo on: {sum/count}");
+
+            // Tässä algoritmi, joka hakee listasta suurimman arvon.
+
+            double value = 0; // Tästä arvosta lähdetään liikkeelle ja tallennetaan suurin arvo.
+
+            for (int i = 0; i < temperaturesList.Count; i++)
+            {
+                // Korvataan oletusarvo 0, ensimmäisellä kierroksella
+                if (i == 0)
+                {
+                    value = temperaturesList[i];
+                }
+
+                // Onko uusi arvo suurempi kuin vanha arvo
+                if (temperaturesList[i] > value)
+                {
+                    value = temperaturesList[i]; // Otetaan talteen isompi arvo
+                }
+            }
+
+            Console.WriteLine($"Listan Suurin arvo on: {value}");
+
+            // Tee algoritmi, joka hakee taulukosta pienimmän arvon
+
+
 
             #endregion
 
