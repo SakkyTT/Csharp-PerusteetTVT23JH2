e7c6fec355089116dcf593ca5c168bee9a113fd9
SakkyTT <teemu.turunen@sakky.fi>
2023-04-18 16:25:31 +0300

    Teoria 6

---
+++ Teoria6(function,class,method,struct,enum)/Teoria6(function,class,method,struct,enum)/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Teoria6_function_class_method_struct_enum_
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            // 1. Funktio
+            #region
+            #endregion
+
+
+            // 2. Luokka
+            #region
+            // 2a. Contructor
+            // 2b. Property
+            // 2c. Metodi
+
+            #endregion
+
+
+            // 3. struct
+            #region
+            #endregion
+
+
+            // 4. enum
+            #region
+            #endregion
+        }
+    }
+}
