6f503ec2d1aa64edb48de21320bd39221a6e131e
SakkyTT <teemu.turunen@sakky.fi>
2023-04-18 17:13:43 +0300

    update1

--- Teoria6(function,class,method,struct,enum)/Teoria6(function,class,method,struct,enum)/Program.cs
+++ Teoria6(function,class,method,struct,enum)/Teoria6(function,class,method,struct,enum)/Program.cs
@@ -6,21 +6,57 @@ using System.Threading.Tasks;
 
 namespace Teoria6_function_class_method_struct_enum_
 {
-    class Program
+    class Program // Luokka
     {
-        static void Main(string[] args)
+        static void Main(string[] args) // Funktio / Metodi
         {
 
-            // 1. Funktio
+            // 1. Funktio / Function
+            //   Funktio on keino kapsuloida tietty toiminnallisuus, jota voidaan käyttää uudelleen
+            //   eri kohdissa projektia.
             #region
+
+            // Funktio ja metodi ovat melkein samoja, erona on niiden sijainti.
+            // Metodi on termi funktiolle, joka on osana luokkaa.
+
+            // Olemme kirjoittaneet koodia luokkaan nimeltä "Program" ja sen funktioon / metodiin
+            // "Main"
+
+            // Funktion / Metodi koodi tulee tiettyyn kohtaa tiedostoa.
+
+            // Kun lähdemme tekemään uutta metodia, se täytyy kirjoittaa samalle tasolle muiden
+            // metodien / funktioiden kanssa, luokan sisälle.
+
+            // Tänne ei voi luoda uutta funktiota, koska olemme jo "Main"-funktion sisällä.
+            // Metodi luotiin "Main"-funktion alapuolelle
+
+            // Täällä voidaan käyttää metodia
+
+            // UUSI TERMI: olio / object
+            // olio on instanssi luokasta. Esimerkki: https://i.stack.imgur.com/lNUAA.png
+            // Oletuksena metodeja voi ainoastaan käyttää, kun luokasta on luotu objekti.
+            // Nyt uutta "printText()"-metodia ei voida käyttää, ellei luoda luokasta "Program"
+            // uutta objektia.
+
+            Program test = new Program(); // Luodaan tämän tiedoston "Program" luokasta objecti
+            test.printText("Hello", 123);
+            test.printText("World", 321);
+
+            // Poikkeuksena staattinen metodi
+            // Jos metodista luodaan staattinen, sitä ovi käyttää suoraan ilman objektia.
+
+            // Esim:
+            Console.WriteLine(""); // Tämä on "Console"-luokan staattinen "WriteLine()"-metodi
+            
+
             #endregion
 
 
-            // 2. Luokka
+            // 2. Luokka / Class
             #region
             // 2a. Contructor
             // 2b. Property
-            // 2c. Metodi
+            // 2c. Metodi / Method
 
             #endregion
 
@@ -33,6 +69,32 @@ namespace Teoria6_function_class_method_struct_enum_
             // 4. enum
             #region
             #endregion
+
+
+            // Pysäytetään sovellus lopussa.
+            Console.WriteLine("Press any key to exit...");
+            Console.ReadKey();
+        } // Main function
+
+        // Luodaan uusi metodi, muiden metodien ulkopuolelle, mutta luokan sisälle.
+
+        // [näkyvyys] [palautettavaDatatyyppi] [metodinNimi] ( [parametrit], pilkulla eroteltuna )
+        // Tämä funktio ottaa vastaan kaksi arvoa: string message ja int number
+        // Metodi kirjoittaa consoliin parametrien sisällön
+        private void printText(string message, int number)
+        {
+            // Private -näkyvyys tarkoittaa, että sitä ei voida käyttää luokan ulkopuolelta.
+            // void -datatyyppi, ei palauta mitään.
+            // parametrit -dataa, joka lähetetään funktioon.
+            Console.WriteLine($"Text: {message}{number}");
         }
-    }
-}
+
+        // Luodaan staattinen metodi
+        // avainsana "static" tulee näkyvyyden ja palautettavan datatyypin väliin.
+        public static int powerTwo (int number1, int number2)
+        {
+
+        }
+
+    } // Class Program
+} // Namespace
