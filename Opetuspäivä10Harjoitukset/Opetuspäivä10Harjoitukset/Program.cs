964d5cf1640e081a3e25edfa8f6f5f2a22571374
SakkyTT <teemu.turunen@sakky.fi>
2023-04-24 18:38:57 +0300

    day 10 update 3

--- Opetuspäivä10Harjoitukset/Opetuspäivä10Harjoitukset/Program.cs
+++ Opetuspäivä10Harjoitukset/Opetuspäivä10Harjoitukset/Program.cs
@@ -23,12 +23,15 @@ namespace Opetuspäivä10Harjoitukset
             List<decimal> userFuelPrice = new List<decimal>();
             // Kun käytetään useaa eri listaa tai taulua, täytyy hallita indeksien käyttö oikein eri listojen välillä.
 
+            decimal[] test = new decimal[1];
+            (decimal, decimal, decimal)[] trips = new(decimal, decimal, decimal)[1];
+
             // Kohta 5, lisätään koko koodi silmukkaan
             while (userEndsLoop == false)
             {
                 Console.Write("Syötä ajettu matka(km): ");
-                //decimal distance = decimal.Parse(Console.ReadLine());
-                userDistances.Add(decimal.Parse(Console.ReadLine()));
+                decimal distance = decimal.Parse(Console.ReadLine());
+                userDistances.Add(distance);
 
                 Console.Write("Syötä ajeneuvon keskikulutus(l/100km): ");
                 decimal averageConsumption = decimal.Parse(Console.ReadLine());
@@ -37,6 +40,8 @@ namespace Opetuspäivä10Harjoitukset
                 Console.Write("Syötä polttoaineen hinta(€/l): ");
                 decimal fuelPrice = decimal.Parse(Console.ReadLine());
                 userFuelPrice.Add(fuelPrice);
+
+                trips[trips.Length - 1] = (distance, averageConsumption, fuelPrice);
 
                 //decimal tripCost = carTripCostCalculator(distance, averageConsumption, fuelPrice);
 
@@ -50,6 +55,13 @@ namespace Opetuspäivä10Harjoitukset
                 if (input == "n") // Käyttäjä lopettaa sovelluksen
                 {
                     userEndsLoop = true; // Kun tämä muuttuja on true, silmukka päättyy
+                }
+                else // Laajennetaan taulukko
+                {
+                    //test = expandArray(test);
+                    //test = expandArray(test);
+                    trips = expandArray(trips); // expandArray palauttaa uuden taulukon, joka on yhtä suurempi kuin
+                                                // Alkuperäinen taulukko, joka annetaan parametrinä                                 
                 }
             }
 
@@ -80,6 +92,20 @@ namespace Opetuspäivä10Harjoitukset
             result = distanceInKilometers * averageConsumption * fuelCost / 100;
 
             return result;
+        }
+
+        private static (decimal, decimal, decimal)[] expandArray((decimal, decimal, decimal)[] originalArray)
+        {
+            // Luodaan uusi taulukko, joka on yhtä suurempi kuin alkuperäinen taulukko
+            (decimal, decimal, decimal)[] newArray = new (decimal, decimal, decimal)[originalArray.Length + 1];
+
+            // Kopioidaan arvot vanhasta taulukosta uuteen taulukkoon
+            for (int i = 0; i < originalArray.Length; i++)
+            {
+                newArray[i] = originalArray[i];
+            }
+
+            return newArray;
         }
 
         #endregion
