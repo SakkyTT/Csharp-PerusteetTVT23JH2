03b05c39669611500d72bd2d766b22b212f1299b
SakkyTT <teemu.turunen@sakky.fi>
2023-05-02 20:19:37 +0300

    day12 done

--- Opetuspäivä12Harjoitukset/Opetuspäivä12Harjoitukset/Program.cs
+++ Opetuspäivä12Harjoitukset/Opetuspäivä12Harjoitukset/Program.cs
@@ -21,14 +21,15 @@ namespace Opetuspäivä12Harjoitukset
             // C# Funktiot: MeterConverter
             // https://moodle.sakky.fi/mod/assign/view.php?id=1159680
 
-            Console.Write("Syötä mitta(m): ");  
+            Console.Write("Syötä mitta(m): ");
             double meters = double.Parse(Console.ReadLine());
             Console.WriteLine($"{meters}m sentteinä on:{MeterConverter.ToCentimeter(meters)}");
             Console.WriteLine($"{meters}m kilometreinä on:{MeterConverter.ToKilometer(meters)}");
 
 
-
-
+            // last digits of a number
+            // https://www.codewars.com/kata/5cd5ba1ce4471a00256930c0/train/csharp
+            // Testaus
             LastDigit(1, 1);
             LastDigit(123767, 4);
             LastDigit(0, 1);
@@ -36,14 +37,6 @@ namespace Opetuspäivä12Harjoitukset
             LastDigit(1234, 0);
             LastDigit(24134, -4);
             LastDigit(1343, 5);
-
-
-
-
-
-
-
-
 
 
             Console.WriteLine();
@@ -95,7 +88,8 @@ namespace Opetuspäivä12Harjoitukset
             return result;
         }
 
-
+        // last digits of a number
+        // https://www.codewars.com/kata/5cd5ba1ce4471a00256930c0/train/csharp
         public static int[] LastDigit(long n, int d)
         {
             // Jos d on negatiivinen, palautetaan tyhjä taulukko
@@ -123,7 +117,7 @@ namespace Opetuspäivä12Harjoitukset
 
             int[] result = new int[d];
             // Aloitetaan taulukon viimeisestä indeksistä
-            int arrayIndex = result.Length - 1; 
+            int arrayIndex = result.Length - 1;
 
             // Kopioidaan uudesta string muuttujasta arvot lopusta alkua kohti
             // Aloitetaan viimeisestä indeksistä
@@ -131,13 +125,49 @@ namespace Opetuspäivä12Harjoitukset
             // Length == 6
             // d == 4
             // 6 - 4 == 2
-            for (int i = asText.Length-1; i >= asText.Length - d; i--)
+            for (int i = asText.Length - 1; i >= asText.Length - d; i--)
             {
                 result[arrayIndex] = int.Parse(asText[i].ToString());
                 arrayIndex--;
             }
 
             return result;
+        }
+
+        // Is he gonna survive?
+        // https://www.codewars.com/kata/59ca8246d751df55cc00014c/train/csharp
+        public static bool Hero(int bullets, int dragons)
+        {
+            // 4, 5 * 2
+            // Tarvittavien luotien määrä on kaksi kertaa dragons
+            // Testataan, onko bullets >= tarvittavien luotien määrä
+            //Do Some Magic...
+            return dragons * 2 <= bullets ? true : false;
+        }
+
+        // C# CodeWars - Find the index of the second occurrence of a letter in a string
+        // https://moodle.sakky.fi/mod/assign/view.php?id=1169035&forceview=1
+        public static int SecondSymbol(string str, char symbol)
+        {
+            int foundInstances = 0; // Otetaan talteen montako kertaa symbol on löytynyt
+
+            for (int i = 0; i < str.Length; i++) // Käydään läpi merkkijono parametri str
+            {
+                if (str[i] == symbol) // Onko merkki sama kuin symboli, jota haetaan
+                {
+                    if (foundInstances < 1) // Ensimmäinen löytö
+                    {
+                        // Otetaan talteen, että symbol on löytynyt
+                        foundInstances++;
+                    }
+                    else // toinen löytö, palautetaan heti indeksi
+                    {
+                        return i;
+                    }
+
+                }
+            }
+            return -1; // Jos ei löydy kahta kertaa
         }
     }
 }
