e132306ecb260f712a52d48006d9b529fc00fe97
SakkyTT <teemu.turunen@sakky.fi>
2023-05-15 17:31:37 +0300

    day 15

---
+++ Opetuspäivä15Harjoitukset/Opetuspäivä15Harjoitukset/Dice.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Opetuspäivä15Harjoitukset
+{
+    class Dice
+    {
+        // Properties
+        private Random rng; // Jos luodaan static, tämä jaetaan kaikkien
+                            // noppa objektien kesken. Jolloin ei saada
+                            // enää samoja arvoja.
+        public int MaxValue { get; set; } // Nopassa on lukuja 1 - tämä arvo
+
+        public int Value { get; set; } // Nopan arvo, joka on ylöspäin
+
+        // Jokin property, jolla voi tehdä "huijaus" nopan
+
+        // Constructors
+        public Dice(int maxValue)
+        {
+            rng = new Random();
+            MaxValue = maxValue;
+            Roll();
+        }
+
+        public Dice(int maxValue, int value) // Kahden parametrin overload
+        {
+            rng = new Random();
+            MaxValue = maxValue;
+            Value = value;
+        }
+
+        // Methods
+
+        // Noppaa "heitetään" ja arvotaan siihen uusi satunnainen arvo
+        public void Roll()
+        {
+            //Random random = new Random();
+            Value = rng.Next(1, MaxValue + 1);
+        }
+
+    }
+}
