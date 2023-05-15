d0c3bd83b53096bdf29f0975214dc906b2b32c39
SakkyTT <teemu.turunen@sakky.fi>
2023-05-15 20:13:09 +0300

    Ostoslista jäänyt kesken

--- Opetuspäivä15Harjoitukset/Opetuspäivä15Harjoitukset/Program.cs
+++ Opetuspäivä15Harjoitukset/Opetuspäivä15Harjoitukset/Program.cs
@@ -10,6 +10,54 @@ namespace Opetuspäivä15Harjoitukset
     {
         static void Main(string[] args)
         {
+            //List<ShoppingList> order = new List<ShoppingList>();
+
+            Console.Write("Mihin kauppaan tehdään ostoslista: ");
+            string shop = Console.ReadLine();
+
+            ShoppingList newOrder = new ShoppingList(shop);
+
+            // Waterfall - Agile Waterfall (scrum) - Agile(pari ohjelmointi)
+
+            do // Käyttäjä lisää ostoksia
+            {
+                Console.Clear();
+                Console.WriteLine($"Valittu kauppa on {newOrder.ShopName}");
+                Console.WriteLine();
+
+                Console.Write("Syötä tuote ja lukumäärä(maito, 3): ");
+                string input = Console.ReadLine();
+                string[] splitInput = input.Split(',');
+                int amount = int.Parse(splitInput[1].Trim());
+
+                // Lisätään tuote listaan käyttäjän syöttämän luvun verran
+                for (int i = 1; i <= amount; i++)
+                {
+                    newOrder.AddProduct(splitInput[0]);
+                }
+
+            } while (true); // TODO this condition
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
             Dice noppa1 = new Dice(6);
             Dice noppa2 = new Dice(6, 1);
 
